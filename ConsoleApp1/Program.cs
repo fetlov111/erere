302fb1d891b48d184d20fa9a8c9b33e2ec3177ac
fetlov111 <145396458+fetlov111@users.noreply.github.com>
2023-10-02 21:20:15 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+ 
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите первое число: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("ведите второе число: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int k = random.Next(x, y);
+            int count = 0;
+            string str;
+
+            Console.WriteLine("угадайчисло в интервале от {0} до {1}\n", x, y);
+
+            do
+            {
+                Console.Write("твой вариант: ");
+                str = Console.ReadLine();
+
+                if (int.Parse(str) < k)
+                    Console.WriteLine("задуманное число больше");
+                if (int.Parse(str) > k)
+                    Console.WriteLine("задуманное число меньше");
+
+                count++;
+            }
+            while (int.Parse(str) != k);
+
+            Console.WriteLine("Кравчик, ты угадал с {0} попытки", count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matrix = new int[10, 10];
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                matrix[i - 1, j - 1] = i * j;
+            }
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write($"{matrix[i, j],2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
